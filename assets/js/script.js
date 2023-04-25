3525058e30d1fad2eea708d2e8a6c8c5d057ef47
monstergog <amn002@ucsd.edu>
2023-04-25 03:43:17 -0700

    started javascript function (incomplete)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 var header = document.querySelector('header');
+var scoreLink = document.querySelector('#scoreLink');
 var timer = document.querySelector('#timer');
 var question = document.querySelector('#question')
 var choices = document.querySelector('#choices');
+var scoreboard = document.querySelector('#scoreboard');
+var start = document.querySelector('#start');
 var answer = document.querySelector('#answer');
 
+start.addEventListener('click', startQuiz);
+scoreLink.addEventListener('click', viewHighScores);
+
+function startQuiz () {
+  start.setAttribute('style', 'display: none;')
+}
+
+function viewHighScores(event) {
+  event.preventDefault();
+  header.setAttribute('style', 'visibility: hidden;');
+}
+
+function clearScores() {
+  if (confirm('Are you sure you want to clear the scoreboard?')) {
+    
+  }
+}
+
+function goBack () {
+  header.setAttribute('style', 'visibility: visible;');
+}
