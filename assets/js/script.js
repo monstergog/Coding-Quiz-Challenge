6b22f1ffb52387bf0340418e79309a157e099cdd
monstergog <amn002@ucsd.edu>
2023-04-25 03:00:40 -0700

    selector setup

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var header = document.querySelector('header');
+var timer = document.querySelector('#timer');
+var question = document.querySelector('#question')
+var choices = document.querySelector('#choices');
+var answer = document.querySelector('#answer');
+
