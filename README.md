a7b2c3806bd98d59b8b5c530a133d9e6a3dfb811
monstergog <amn002@ucsd.edu>
2023-04-26 02:41:05 -0700

    added quiz question objects

--- README.md
+++ README.md
@@ -20,3 +20,8 @@ THEN the game is over
 
 WHEN the game is over
 THEN I can save my initials and score
+
+
+
+
+https://www.interviewbit.com/javascript-mcq/
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,107 @@ var start = document.querySelector('#start');
 var answer = document.querySelector('#answer');
 var submit = document.querySelector('#submit');
 
+var question1 = {
+  q: 'Javascript is a ___ language?',
+  a1: 'Object-oriented',
+  a2: 'Object-based',
+  a3: 'Procedural',
+  a4: 'None of the Above',
+  correct: 1
+}
+
+var question2 = {
+  q: 'Which of the following keywords is used to define a variable in Javascript?',
+  a1: 'var',
+  a2: 'let',
+  a3: 'Both A and B',
+  a4: 'None of the Above',
+  correct: 3
+}
+
+var question3 = {
+  q: 'Which of the following methods is used to access HTML elements using Javascript?',
+  a1: 'getElementbyId()',
+  a2: 'getElementsbyClassName()',
+  a3: 'Both A and B',
+  a4: 'None of the Above',
+  correct: 3
+}
+
+var question4 = {
+  q: 'Upon encountering empty statements, what does the Javascript Interpreter do?',
+  a1: 'Throws and error',
+  a2: 'Ignores the statements',
+  a3: 'Gives a warning',
+  a4: 'None of the Above',
+  correct: 2
+}
+
+var question5 = {
+  q: 'Which of the following methods can be used to display data in some form using Javascript?',
+  a1: 'document.write()',
+  a2: 'console.log()',
+  a3: 'window.alert()',
+  a4: 'All of the Above',
+  correct: 4
+}
+
+var question6 = {
+  q: 'How can a datatype be declared to be a constant type?',
+  a1: 'const',
+  a2: 'var',
+  a3: 'let',
+  a4: 'constant',
+  correct: 1
+}
+
+var question7 = {
+  q: 'When the switch statement matches the expression with the given labels, how is the comparison done?',
+  a1: 'Both the datatype and the result of the expression are compared',
+  a2: 'Only the datatype of the expression is compared',
+  a3: 'Only the value of the expression is compared',
+  a4: 'None of the Above',
+  correct: 1
+}
+
+var question8 = {
+  q: 'What keyword is used to check whether a given property is valid or not?',
+  a1: 'in',
+  a2: 'is in',
+  a3: 'exists',
+  a4: 'lies',
+  correct: 1
+}
+
+var question9 = {
+  q: 'What is the use of the <noscript> tag in Javascript?',
+  a1: 'The contents are displayed by non-JS-based browsers',
+  a2: 'Clears all the cookies and cache',
+  a3: 'Both A and B',
+  a4: 'None of the Above',
+  correct: 1
+}
+
+var question10 = {
+  q: 'Which function is used to serialize an object into a JSON string in Javascript?',
+  a1: 'stringify()',
+  a2: 'parse()',
+  a3: 'convert()',
+  a4: 'None of the Above',
+  correct: 1
+}
+
 start.addEventListener('click', startQuiz);
+choices.addEventListener('click', quizChoice);
 scoreLink.addEventListener('click', viewHighScores);
 submit.addEventListener('click', submitScore);
 
 function startQuiz () {
   start.setAttribute('style', 'display: none;')
+  quizChoice();
+}
 
+function timerFunction () {
   var timeLeft = 2;
   var timeInterval = setInterval( function () {
     timeLeft--;
@@ -27,6 +121,18 @@ function startQuiz () {
     }
   }
   , 1000)
+}
+
+function quizChoice () {
+  choices.textContent = '';
+  choices.setAttribute('style', 'display: block;');
+
+  var li = document.createElement('li');
+  var button = document.createElement('button');
+
+  li.setAttribute('data-index', '1');
+  button.textContent = 'testing';
+  choices.appendChild(li).appendChild(button);
 }
 
 function submitScore (event) {
